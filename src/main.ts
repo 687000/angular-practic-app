9d710ddb72e9b1b96d1a76d0e311ed1037279fe7
jiani liu <liujianiljn@gmail.com>
2023-07-15 10:19:07 -0700

    add bootstrap module

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-platformBrowserDynamic(); 
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err=>console.log(err));
