2c4db7b919e1e4b8e3813191aaac256300fcfdc4
jiani liu <liujianiljn@gmail.com>
2023-07-20 17:40:39 -0700

    use templateurl and styleurls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>hello world</h1>`
+  //template: `<div>hellow world`
+  templateUrl: './app.component.html',
+  //style
+  styleUrls:['./app.component.css']
 })
 export class AppComponent { 
   title = 'angular-practice-app';
